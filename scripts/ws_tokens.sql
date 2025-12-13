bb05d35046aee27155cd73e5ad8527b771bbed9e
Pantelis Roditis <proditis@echothrust.com>
2025-12-13 22:26:24 +0200

    update sample schema remove revoke

--- scripts/ws_tokens.sql
+++ scripts/ws_tokens.sql
@@ -1,16 +1,15 @@
 -- ws_tokens.sql
-CREATE TABLE IF NOT EXISTS ws_tokens (
+CREATE TABLE IF NOT EXISTS ws_token (
     token        VARCHAR(64) PRIMARY KEY,
-    subject_id   VARCHAR(64) NOT NULL,
+    subject_id   VARCHAR(64) NOT NULL unique,
     expires_at   DATETIME NOT NULL,
-    revoked      BOOLEAN DEFAULT 0,
     is_server    BOOLEAN DEFAULT 0
 );
 
 -- Player token
-INSERT INTO ws_tokens (token, subject_id, expires_at, is_server)
+INSERT INTO ws_token (token, subject_id, expires_at, is_server)
 VALUES ('player123token', 'player1', DATETIME('now', '+1 day'), 0);
 
 -- Server token
-INSERT INTO ws_tokens (token, subject_id, expires_at, is_server)
+INSERT INTO ws_token (token, subject_id, expires_at, is_server)
 VALUES ('server123token', 'server1', DATETIME('now', '+1 day'), 1);
